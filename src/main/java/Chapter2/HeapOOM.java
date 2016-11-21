3fbc58e54a3caa180208ea3bbd8a5cb23a20a64d
Knight_JNXU <1990785022@qq.com>
2016-11-21 21:06:23 +0800

    chapter2

---
+++ src/main/java/Chapter2/HeapOOM.java
@@ -0,0 +1,19 @@
+package Chapter2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Knigh on 2016/11/21.
+ */
+public class HeapOOM {
+    static class OOMObject{
+    }
+
+    public static void main(String[] args){
+        List<OOMObject> list = new ArrayList<OOMObject>();
+        while (true){
+            list.add(new OOMObject());
+        }
+    }
+}
