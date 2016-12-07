28e808e3109969fa98618aefac8327a4416e5ff6
Knight-JXNU <1990785022@qq.com>
2016-12-07 13:37:52 +0800

    大对象直接放入老年代

--- src/main/java/Chapter3/BigObject.java
+++ src/main/java/Chapter3/BigObject.java
@@ -10,7 +10,7 @@ package Chapter3;
  * -XX:+PrintGCDetails 打印GC详细情况，比如 Heap PsYoungGen xxxxx
  * -XX:SurvivorRatio=8 设置 Edon:Survivor:Survivor = 8:1:1
  * -XX:PretenureSizeThreshold=3145728 设置对象多大（字节）时可以直接进入老年代，这里3145728是3M
- * -XX:+UseParNewGC 设置使用ParNew+Serial Old收集器，因为 PretenureSizeThreshold只对 PareNew 和 Serial有效
+ * -XX:+UseParNewGC 设置使用ParNew+Serial Old收集器，因为 PretenureSizeThreshold只对 ParNew 和 Serial有效
  */
 public class BigObject {
     private static final int _1MB=1024*1024;
