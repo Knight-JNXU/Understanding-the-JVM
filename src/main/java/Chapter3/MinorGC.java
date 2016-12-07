bba027778ab37f3cd9265740fa06e2918748b843
Knight-JXNU <1990785022@qq.com>
2016-12-07 10:30:34 +0800

    Minor GC

--- src/main/java/Chapter3/MinorGC.java
+++ src/main/java/Chapter3/MinorGC.java
@@ -2,9 +2,10 @@ package Chapter3;
 
 /**
  * Created by Knight_JXNU on 2016/12/6.
+ * jvm options:-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
  */
 public class MinorGC {
-    private static final int _1MB = 1027*1024;
+    private static final int _1MB = 1024*1024;
 
     public static void main(String[] args){
         testAllocation();
@@ -16,5 +17,6 @@ public class MinorGC {
         allocation2 = new byte[2*_1MB];
         allocaiton3 = new byte[2*_1MB];
         allocation4 = new byte[2*_1MB];  //出现一次 Minor GC
+        System.out.println("end");
     }
 }
